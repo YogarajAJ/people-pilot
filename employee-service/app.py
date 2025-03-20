ebdb275745fb5ba5c95dc4fc667e6e77ca22f196
santhoshsara <santhoshsara1010@gmail.com>
2025-03-20 11:08:40 +0530

    changed some set of codes

--- employee-service/app.py
+++ employee-service/app.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 app.register_blueprint(employee_blueprint, url_prefix="/api/employee")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5002)
